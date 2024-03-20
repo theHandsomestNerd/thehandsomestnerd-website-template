5d612b6e8b40501d440de44b50721e0732884e17
Mr. Singleton <terrell.singleton@gmail.com>
2024-03-20 12:11:08 -0400

    added essential web files robots and sitemap; fixed meta tags not being picked up

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -23,31 +23,31 @@ Promise.polyfill();
 
 const app = express();
 
-const corsOptionsDelegate = (req:any, callback:any) => {
-  logClient.log("CORS", "NOTICE", "checking allowlist", {origin: req.header("Origin")});
-  // let corsOptions;
-  // if (allowlist.indexOf(req.header("Origin")) !== -1) {
-  //   logClient.log("CORS", LogLevels.NOTICE, "origin in allowlist", {origin: req.header("Origin"), allowlist});
-  //   corsOptions = {origin: allowlist}; // reflect (enable) the requested origin in the CORS response
-  // } else {
-  //   logClient.log("CORS", LogLevels.NOTICE, "origin NOT in allowlist", {origin: req.header("Origin"), allowlist});
-  //   corsOptions = {origin: false}; // disable CORS for this request
-  // }
-  const corsOptions = {origin: true};
+const corsOptionsDelegate = (req: any, callback: any) => {
+    logClient.log("CORS", "NOTICE", "checking allowlist", {origin: req.header("Origin")});
+    // let corsOptions;
+    // if (allowlist.indexOf(req.header("Origin")) !== -1) {
+    //   logClient.log("CORS", LogLevels.NOTICE, "origin in allowlist", {origin: req.header("Origin"), allowlist});
+    //   corsOptions = {origin: allowlist}; // reflect (enable) the requested origin in the CORS response
+    // } else {
+    //   logClient.log("CORS", LogLevels.NOTICE, "origin NOT in allowlist", {origin: req.header("Origin"), allowlist});
+    //   corsOptions = {origin: false}; // disable CORS for this request
+    // }
+    const corsOptions = {origin: true};
 
-  callback(null, corsOptions); // callback expects two parameters: error and options
+    callback(null, corsOptions); // callback expects two parameters: error and options
 };
 app.use(cors(corsOptionsDelegate));
 // app.use(cors());
 
 admin.initializeApp({
-  // credential: admin.credential.cert(serviceAccount),
+    // credential: admin.credential.cert(serviceAccount),
 });
 
 // Custom logger to keep log messages together in one json
-const Logger = function(req: any, res: any, next: any) {
-  logClient.createLogger(req, res, next);
-  next();
+const Logger = function (req: any, res: any, next: any) {
+    logClient.createLogger(req, res, next);
+    next();
 };
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
@@ -56,33 +56,33 @@ const Logger = function(req: any, res: any, next: any) {
 
 const builder = imageUrlBuilder(sanityClient);
 export const urlFor = (source: any) => {
-  return builder.image(source);
+    return builder.image(source);
 };
 
 app.use(Logger);
 // https://blog.logrocket.com/adding-dynamic-meta-tags-react-app-without-ssr/
 app.get("/*", (req, res, next) => {
-  logClient.log("server-side", "NOTICE",
-      "Hello from the Server Siiiiiide", req.params);
+    logClient.log("server-side", "NOTICE",
+        "Hello from the Server Siiiiiide", req.params);
 
-  logClient.log("server-side", "NOTICE",
-      "index path", indexPath);
+    logClient.log("server-side", "NOTICE",
+        "index path", indexPath);
 
-  serveIndexFile(req, res);
+    serveIndexFile(req, res);
 });
 
 app.get("/", (req, res, next) => {
-  logClient.log("server-side", "NOTICE",
-      "Serving Index instead of hosting", req.params);
+    logClient.log("server-side", "NOTICE",
+        "Serving Index instead of hosting", req.params);
 
-  serveIndexFile(req, res);
+    serveIndexFile(req, res);
 });
 
 app.get("/index.html", (req, res, next) => {
-  logClient.log("server-side", "NOTICE",
-      "Serving Index instead of hosting", req.params);
+    logClient.log("server-side", "NOTICE",
+        "Serving Index instead of hosting", req.params);
 
-  serveIndexFile(req, res);
+    serveIndexFile(req, res);
 });
 
 console.log(__dirname + " " + "../../../../" + "build");
@@ -92,144 +92,161 @@ const prodIndexPath: string[] = [];
 const indexPathParts = process.env.SANITY_DB === "production" ? prodIndexPath : devIndexPath;
 const files = fs.readdirSync(path.resolve(__dirname, ...indexPathParts));
 
-const indexPath = path.resolve(__dirname, ...indexPathParts, "index.html");
+const indexPath = path.resolve(__dirname, ...indexPathParts, "./index.html");
 
 console.log(path.resolve(__dirname, ...indexPathParts), files);
 
 
 const serveIndexFile = (req: any, res: any) => {
-  fs.readFile(indexPath, "utf8", async (err, htmlData) => {
-    if (err) {
-      console.error("Error during file reading", err);
-      return res.status(404).end();
-    }
+    fs.readFile(indexPath, "utf8", async (err, htmlData) => {
+        if (err) {
+            console.error("Error during file reading", err);
+            return res.status(404).end();
+        }
 
-    // TODO get info from sanity info
-    const params: any = req.params;
-    const tokenizedParams = params["0"].split("/");
+        const params: any = req.params;
+        let pageSlug = "home"
+        try {
+            const tokenizedParams = params["0"].split("/");
 
-    let pageSlug = tokenizedParams[tokenizedParams.length - 1];
+            pageSlug = tokenizedParams[tokenizedParams.length - 1];
 
-    if (!pageSlug) {
-      pageSlug = "home";
-    }
+            if (!pageSlug || pageSlug === undefined || pageSlug === "undefined" || tokenizedParams === undefined || params["0"] === "undefined" || params["0"] === undefined || tokenizedParams.length === 0 || tokenizedParams[tokenizedParams.length - 1] === "undefined" || tokenizedParams[tokenizedParams.length - 1] === undefined) {
+                pageSlug = "home";
+            }
 
-    logClient.log("server-side", "NOTICE",
-        "Loading this page from sanity", pageSlug);
-    try {
-      const pageFromSanity: any = await cmsClient.fetchPage(pageSlug);
+            logClient.log("server-side", "NOTICE",
+                "Loading this page from sanity", pageSlug);
+            const pageFromSanity: any = await cmsClient.fetchPage(pageSlug);
 
-      // console.log("IMAGE URL", pageFromSanity.metaImage && urlFor(pageFromSanity.metaImage).url()?.replace("undefined", process.env.SANITY_DB ?? "development"));
-      const page = {
-        ogTitle: pageFromSanity?.title,
-        description: pageFromSanity?.description,
-        ogDescription: pageFromSanity?.description,
-        ogImage: pageFromSanity?.metaImage && builder.image(pageFromSanity.metaImage).url()?.replace("undefined", process.env.SANITY_DB ?? "development"),
-      };
+            logClient.log("server-side", "NOTICE",
+                "Page from Sanity", pageFromSanity)
+            // console.log("IMAGE URL", pageFromSanity.metaImage && urlFor(pageFromSanity.metaImage).url()?.replace("undefined", process.env.SANITY_DB ?? "development"));
+            const page = {
+                ogTitle: pageFromSanity?.title,
+                description: pageFromSanity?.description,
+                ogDescription: pageFromSanity?.description,
+                ogUrl: pageFromSanity?.website,
+                ogImage: pageFromSanity?.metaImage && builder.image(pageFromSanity.metaImage).url()?.replace("undefined", process.env.SANITY_DB ?? "development"),
+            };
 
-      logClient.log("server-side", "NOTICE",
-          "MetaTag Data", page);
+            logClient.log("server-side", "NOTICE",
+                "MetaTag Data", page);
 
-      htmlData = htmlData.replace(
-          "<title>React App</title>",
-          `<title>${page.ogTitle}</title>`)
-          .replace("__META_OG_TITLE__", page.ogTitle ?? "")
-          .replace("__META_OG_DESCRIPTION__", page.description ?? "")
-          .replace("__META_DESCRIPTION__", page.ogDescription ?? "")
-          .replace("__META_OG_IMAGE__", page.ogImage ?? "");
+            htmlData = htmlData.replace(
+                "<title>React App</title>",
+                `<title>${page.ogTitle ?? ""}</title>`)
+                .replace("__META_OG_URL__", page.ogTitle ?? "")
+                .replace("__META_OG_TITLE__", page.ogTitle ?? "")
+                .replace("__META_OG_TITLE__", page.ogTitle ?? "")
+                .replace("__META_OG_TITLE__", page.ogTitle ?? "")
+                .replace("__META_DESCRIPTION__", page.description ?? "")
+                .replace("__META_DESCRIPTION__", page.ogDescription ?? "")
+                .replace("__META_DESCRIPTION__", page.ogDescription ?? "")
+                .replace("__META_DESCRIPTION__", page.ogDescription ?? "")
+                .replace("__META_OG_IMAGE__", page.ogImage ?? "")
+                .replace("__META_OG_IMAGE__", page.ogImage ?? "")
+                .replace("__META_OG_IMAGE__", page.ogImage ?? "");
 
-      return res.send(htmlData);
-    } catch (e: any) {
-      logClient.log("server-side", "ERROR",
-          "Error Fetching Page", {pageSlug, error: e.message});
-      return res.send({status: "404", message: e.message});
-    }
-  });
+            logClient.log("server-side", "NOTICE",
+                "fetched and modified page ",htmlData)
+
+            return res.send(htmlData);
+            // res.render('index', {
+            // @ts-ignore
+                // headTags: ReactDOMServer.renderToStaticMarkup(htmlData),
+            // })
+        } catch (e: any) {
+            logClient.log("server-side", "ERROR",
+                "Error Fetching Page", {pageSlug, error: e.message});
+            return res.send({status: "404", message: e.message});
+        }
+    });
 };
 
 
 app.post("/send-email-resume",
-    async (req, functionRes:any) => {
-      const reqBody = JSON.parse(req.body);
+    async (req, functionRes: any) => {
+        const reqBody = JSON.parse(req.body);
 
-      logClient.log("send-email-address", "NOTICE",
-          "Request to collect an email address and send them an email", reqBody.email);
+        logClient.log("send-email-address", "NOTICE",
+            "Request to collect an email address and send them an email", reqBody.email);
 
-      try {
-        const response = await cmsClient.createColdLead({
-          email: reqBody.email,
-          leadPhone: reqBody.leadPhone,
-          leadMessage: reqBody.leadMessage,
-          leadName: reqBody.leadName,
-          source: reqBody.source,
-        });
-        console.log("Cold Lead Created before Resume send.", response);
-        // functionRes.send({status: "200", response, email: reqBody.email, message: "Thank you! We will talk soon."});
-      } catch (e) {
-        logClient.log("collect-email-address", "ERROR",
-            "Could not create Lead", {email: reqBody.email});
-        // functionRes.send({status: "400", e});
-      }
+        try {
+            const response = await cmsClient.createColdLead({
+                email: reqBody.email,
+                leadPhone: reqBody.leadPhone,
+                leadMessage: reqBody.leadMessage,
+                leadName: reqBody.leadName,
+                source: reqBody.source,
+            });
+            console.log("Cold Lead Created before Resume send.", response);
+            // functionRes.send({status: "200", response, email: reqBody.email, message: "Thank you! We will talk soon."});
+        } catch (e) {
+            logClient.log("collect-email-address", "ERROR",
+                "Could not create Lead", {email: reqBody.email});
+            // functionRes.send({status: "400", e});
+        }
 
-      try {
-        const response = await sendGridClient.sendLeadEmail(reqBody.email);
-        console.log(`Resume send attempt to ${reqBody.email}:`, response);
-        functionRes.send({status: "200", response, email: reqBody.email, message: "Success"});
-      } catch (e) {
-        logClient.log("collect-email-address", "ERROR",
-            "Could not create Lead", {email: reqBody.email});
-        functionRes.error({status: "400", e});
-      }
+        try {
+            const response = await sendGridClient.sendLeadEmail(reqBody.email);
+            console.log(`Resume send attempt to ${reqBody.email}:`, response);
+            functionRes.send({status: "200", response, email: reqBody.email, message: "Success"});
+        } catch (e) {
+            logClient.log("collect-email-address", "ERROR",
+                "Could not create Lead", {email: reqBody.email});
+            functionRes.error({status: "400", e});
+        }
     });
 
 app.post("/collect-email-address",
-    async (req, functionRes:any) => {
-      const reqBody = JSON.parse(req.body);
+    async (req, functionRes: any) => {
+        const reqBody = JSON.parse(req.body);
 
-      logClient.log("collect-email-address", "NOTICE",
-          "Request to collect an email address", reqBody.email);
+        logClient.log("collect-email-address", "NOTICE",
+            "Request to collect an email address", reqBody.email);
 
-      try {
-        const response = await cmsClient.createColdLead({
-          email: reqBody.email,
-          leadPhone: reqBody.leadPhone,
-          leadMessage: reqBody.leadMessage,
-          leadName: reqBody.leadName,
-          source: reqBody.source,
-        });
-        functionRes.send({status: "200", response, email: reqBody.email});
-      } catch (e) {
-        logClient.log("collect-email-address", "ERROR",
-            "Could not create Lead", {email: reqBody.email});
-        functionRes.error({status: "400", e});
-      }
+        try {
+            const response = await cmsClient.createColdLead({
+                email: reqBody.email,
+                leadPhone: reqBody.leadPhone,
+                leadMessage: reqBody.leadMessage,
+                leadName: reqBody.leadName,
+                source: reqBody.source,
+            });
+            functionRes.send({status: "200", response, email: reqBody.email});
+        } catch (e) {
+            logClient.log("collect-email-address", "ERROR",
+                "Could not create Lead", {email: reqBody.email});
+            functionRes.error({status: "400", e});
+        }
     });
 
 app.post("/create-contact-us",
-    async (req, functionRes:any) => {
+    async (req, functionRes: any) => {
         logClient.log("create-contact-us", "NOTICE",
             "Request body raw", req.body);
-    const reqBody = JSON.parse(req.body);
+        const reqBody = JSON.parse(req.body);
 
-      logClient.log("create-contact-us", "NOTICE",
-          "Request to create contact us", reqBody);
-    //
-      try {
-        const response = await cmsClient.createContactUs({
-          ...reqBody
-        });
-        functionRes.send({status: "200", response, email: reqBody.email});
-      } catch (e) {
-        logClient.log("create-contact-us", "ERROR",
-            "Could not create Contact Us", {email: reqBody.email});
-        functionRes.error({status: "400", e});
-      }
+        logClient.log("create-contact-us", "NOTICE",
+            "Request to create contact us", reqBody);
+        //
+        try {
+            const response = await cmsClient.createContactUs({
+                ...reqBody
+            });
+            functionRes.send({status: "200", response, email: reqBody.email});
+        } catch (e) {
+            logClient.log("create-contact-us", "ERROR",
+                "Could not create Contact Us", {email: reqBody.email});
+            functionRes.error({status: "400", e});
+        }
     });
 
-app.use(express.static(
-    path.resolve(__dirname, "../../../../", "build"),
-    {maxAge: "30d", index: "blah.txt"},
-));
+// app.use(express.static(
+//     path.resolve(__dirname, "../../", "dist/cjs"),
+//     {maxAge: "30d", index: "blah.txt"},
+// ));
 
 exports.app = functions.https.onRequest(app);
 
